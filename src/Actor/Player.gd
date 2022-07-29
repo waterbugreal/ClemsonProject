b9329aa4272975afd44d47a4ad8471d12ec95041
Rehabescapi <jasonmlehmann@gmail.com>
2022-07-29 13:09:10 -0500

    Getting Collision to work

--- src/Actor/Player.gd
+++ src/Actor/Player.gd
@@ -1,30 +1,72 @@
 extends KinematicBody2D
 signal hit
 
-export var speed = 400 # How fast the player will move (pixels/sec).
+const FLOOR_DETECT_DISTANCE = 20.0
+export var speed = Vector2(150.0, 350.0)
+onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
+
+const FLOOR_NORMAL = Vector2.UP
+
+var _velocity = Vector2.ZERO
+
+# _physics_process is called after the inherited _physics_process function.
+# This allows the Player and Enemy scenes to be affected by gravity.
+
+
+
+
+onready var platform_detector = $PlatformDetector
+
+export(String) var action_suffix = ""
+
+ # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		
+		
+		
+		
+	var direction = get_direction()
+	print(direction)
+	
+	var is_jump_interrupted = Input.is_action_just_released("jump" + action_suffix) and _velocity.y < 0.0
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+
+	 
+	var snap_vector = Vector2.ZERO
+	if direction.y == 0.0:
+		snap_vector = Vector2.DOWN * FLOOR_DETECT_DISTANCE
+	var is_on_platform = platform_detector.is_colliding()
+	_velocity.y += gravity * delta
+	_velocity = move_and_slide_with_snap(
+		_velocity, snap_vector, FLOOR_NORMAL, not is_on_platform, 4, 0.9, false
+	)
+	
+	if is_on_platform == true:
+		print("woo")
+	
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		print("I collided with ", collision.collider.name)
+
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	#position += velocity * delta
+	#position.x = clamp(position.x, 0, screen_size.x)
+	#position.y = clamp(position.y, 0, screen_size.y)
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
@@ -35,12 +77,27 @@ func _process(delta):
 		$AnimatedSprite.flip_v = velocity.y > 0
 
 
-func _on_Player_body_entered(body):
-	pass # Replace with function body.
-	hide() # Player disappears after being hit.
-	emit_signal("hit")
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right" + action_suffix) - Input.get_action_strength("move_left" + action_suffix),
+		-1 if is_on_floor() and Input.is_action_just_pressed("jump" + action_suffix) else 0
+	)
+
+func calculate_move_velocity(
+		linear_velocity,
+		direction,
+		speed,
+		is_jump_interrupted
+	):
+	var velocity = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		# Decrease the Y velocity by multiplying it, but don't set it to 0
+		# as to not be too abrupt.
+		velocity.y *= 0.6
+	return velocity
 
 func start(pos):
 	position = pos
